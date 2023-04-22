f57ae9d2901dea7415fa3372542d27ae4f1f836c
Joaquin Schanzenbach <joacohao1@gmail.com>
2023-04-22 13:01:52 -0300

    Ejercicio: determinar si es impar o no

---
+++ esparono.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("Ingrese un numero: ");
+    scanf("%d",&numero);
+    if (numero % 2 == 0){
+        printf("El numero es par");
+
+    }else {
+        printf("El numero es impar");
+    }
+
+    return 0;
+}
