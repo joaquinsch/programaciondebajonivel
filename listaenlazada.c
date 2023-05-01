e5aecdb78da34e055df6cfc1d26a6eda3e6946f8
Joaquin Schanzenbach <joacohao1@gmail.com>
2023-04-30 21:06:50 -0300

    Lista enlazada

---
+++ listaenlazada.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+typedef struct structNodo{
+    int valor;
+    struct structNodo *proximo;
+
+}Nodo;
+
+Nodo* crearNodo(int valor) {
+    Nodo* nuevo = malloc(sizeof(Nodo));
+    nuevo->valor = valor;
+    nuevo->proximo = NULL;
+    return nuevo;
+}
+
+//Agregar al principio
+Nodo* prepend(Nodo* head,int valor){
+    Nodo* nuevo = crearNodo(valor);
+    nuevo->proximo = head;
+    return nuevo;
+}
+// Agregar al final
+Nodo* append(Nodo* head, int val){
+    Nodo* nuevoNodo = crearNodo(val);
+    if(head == NULL){
+        return nuevoNodo;
+    }else{
+        Nodo* actual = head;
+        while (actual->proximo != NULL){
+            actual = actual->proximo;
+        }
+        actual->proximo = nuevoNodo;
+    }
+    return head;
+}
+// Cantidad de elementos en la lista
+int tamanio(Nodo* head){
+    int contador = 0;
+    while(head != NULL){
+        contador++;
+        head = head->proximo;
+    }
+    return contador;
+}
+// Devuelve el indice del primer nodo que tiene el valor dado,
+// y devuelve -1  si no se encontró
+int buscar(Nodo* head,int valor){
+    if(head == NULL){
+        return -1;
+    }
+    Nodo* actual = head;
+    int pos = 0;
+    while(actual != NULL){
+        if(actual->valor == valor){
+            return pos;
+        }
+        pos++;
+        actual = actual->proximo;
+    }
+    return -1;
+}
+// Elimina el primer nodo que tenga el valor dado
+Nodo* eliminar(Nodo* head,int valor){
+    if(head == NULL){
+        exit(0);
+    }
+
+    if(head->valor == valor){
+        head = head->proximo;
+        return head;
+    }
+    Nodo* actual = head;
+    while(actual->proximo != NULL){
+        if(actual->proximo->valor == valor){
+            actual->proximo = actual->proximo->proximo;
+
+
+        }
+        actual = actual->proximo;
+    }
+    return head;
+}
+
+void imprimirLista(Nodo* head){
+    while(head != NULL){
+        printf("%d - ", head->valor);
+        head = head->proximo;
+    }
+    printf("\n");
+}
+
+int main(){
+    Nodo* lista = NULL;
+
+    lista = append(lista, 3);
+    lista = append(lista, 4);
+    lista = append(lista, 23);
+    lista = append(lista, 6);
+
+    lista = prepend(lista, 1);
+    lista = prepend(lista, 30);
+
+    lista = eliminar(lista, 1);
+
+    imprimirLista(lista);
+
+    printf("La cantidad de nodos de la lista es: %d \n", tamanio(lista));
+    printf("Se encontro en la posicion: %d\n" ,buscar(lista, 4));
+
+    return 0;
+
+    // Prueba
+    /* Nodo* lista = NULL;
+     Nodo n1, n2, n3;
+     n1.valor = 13;
+     n2.valor = 2;
+     n3.valor = 1;
+
+     lista = &n1;
+     n1.proximo = &n2;
+     n2.proximo = &n3;
+     n3.proximo = NULL;
+     */
+
+}
